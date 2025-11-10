07d9509cfbb66bc7bab7fcc4513a966adf49f372
itzzmeakhi <akhilmallidi.98@gmail.com>
2025-11-10 10:45:58 +0530

    feat: integrate minikube deployment

--- customer-service/src/server.js
+++ customer-service/src/server.js
@@ -4,6 +4,6 @@ import app from "./app.js";
 const PORT = process.env.PORT || 5001;
 connectDB();
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Customer Service running on port ${PORT}`);
 });
